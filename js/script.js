29020e452748541d001a6e6040c520f1043dab68
robinrakibul <robinrakibulhassan@gmail.com>
2022-02-26 21:01:06 +0600

    document element accessed

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const   quoteText = document.getElementById('quote-text'),
+        quoteTags = document.getElementById('quote-tags'),
+        quoteAuther = document.getElementById('quote-auther'),
+        generateQuoteBtn = document.getElementById('generate-quote-btn');
