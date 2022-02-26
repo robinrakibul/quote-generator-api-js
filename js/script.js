31f5a228843e60b6d50cd10f99a5259479f33bc2
robinrakibul <robinrakibulhassan@gmail.com>
2022-02-26 21:13:22 +0600

    project finished, fixed bugs

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 const   quoteText = document.getElementById('quote-text'),
         quoteTags = document.getElementById('quote-tags'),
-        quoteAuther = document.getElementById('quote-auther'),
+        quoteAuthor = document.getElementById('quote-author'),
         generateQuoteBtn = document.getElementById('generate-quote-btn');
+
+// fetching API function
+function randomQuote(){
+    fetch('https://api.quotable.io/random')
+    .then(res=>res.json())
+    .then(data=> {
+        quoteText.innerText = data.content;
+        quoteTags.innerText = data.tags;
+        quoteAuthor.innerText = `- ${data.author}`;
+    });
+}
+randomQuote();
+// Gen New Quote Button
+generateQuoteBtn.addEventListener('click',function(){
+    randomQuote();
+})
+
